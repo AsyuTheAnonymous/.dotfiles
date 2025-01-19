f1261797a6b5fcbf3032cd8d31f7c556f2b3bba3
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-01-19 17:56:22 -0600

    Various changes

--- packages/gaming.nix
+++
@@ -1,9 +0,0 @@
-{config, pkgs, lib, ... }:
-
-
-{
-  environment.systemPackages = with pkgs; [
-    steam
-    lutris
-  ];
-}
--- support/steam.nix
+++ support/steam.nix
@@ -10,6 +10,7 @@
 
   environment.systemPackages = with pkgs; [
     protonup-qt
+    lutris
   ];
 
 }
