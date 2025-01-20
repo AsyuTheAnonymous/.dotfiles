ad8c5269f1a4d4d7a3590bbf5e3afaca8984b26f
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-01-19 22:36:06 -0600

    shell

--- shell.nix
+++ shell.nix
@@ -5,21 +5,19 @@
 
     # ZSH Default Shell
   environment.shells = with pkgs; [ zsh ];
+  # May move this to a better location
   users.defaultUserShell = pkgs.zsh;
-  programs.zsh.enable = true;
-  # ZSH Aliases
-  programs.zsh.shellAliases = {
-    flake = "nix flake update";
-    rebuild = "sudo nixos-rebuild switch --flake .";
-    home = "home-manager switch --flake .";
-    clean = "nix-env --delete-generations 5d";
-    cleanhm = "home-manager expire-generations '-5 days'";
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      flake = "nix flake update";
+      rebuild = "sudo nixos-rebuild switch --flake .";
+      home = "home-manager switch --flake .";
+      clean = "nix-env --delete-generations 5d";
+      cleanhm = "home-manager expire-generations '-5 days'";
+    };
   };
 
-
-
-
-
-
-
+ 
 }
