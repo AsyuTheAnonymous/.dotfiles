9f4aa131cc1040d1ab221c21b0ab0ff6c1d3026d
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-01-20 13:49:50 -0600

    Removed osu

--- configuration.nix
+++ configuration.nix
@@ -60,7 +60,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.asyu = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "input" "libvirtd" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "input" "libvirtd" "nix-users"]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
     ];
   };
