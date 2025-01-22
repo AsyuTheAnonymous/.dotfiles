a35f73e8a8a211fc7366582f8db65d46d92a03c4
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-01-22 11:46:19 -0600

    Fixed paths

--- system/settings.nix
+++ system/settings.nix
@@ -2,13 +2,13 @@
 
 {
   imports = [
-    ./system/drives.nix
-    ./system/nvidia.nix
-    ./system/pipewire.nix
+    ./drives.nix
+    ./nvidia.nix
+    ./pipewire.nix
     # sddm is declared in ./desktop/hypr/hypr.nix
     #./system/sddm.nix
-    ./system/shell.nix
-    ./system/bootloader/grub.nix
+    ./shell.nix
+    ./bootloader/grub.nix
    #./system/bootloader/sysd.nix
   ];
 }
