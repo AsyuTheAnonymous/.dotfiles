73c984d8b73e679f74e66e2c38645205bacae469
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-01-23 14:30:02 -0600

    Ollama configured!

--- support/ai/ollama.nix
+++ support/ai/ollama.nix
@@ -4,14 +4,15 @@
 {
   environment.systemPackages = with pkgs; [
     ollama
-
   ];
-
-
-
-
-
-
-
-
+    # Ollama + Location
+  services.ollama = {
+    enable = true;
+    models = "/run/media/asyu/Vault/Models";
+  };
+  # Ollama Web Ui
+  services.nextjs-ollama-llm-ui = {
+    enable = true;
+    ollamaUrl = "http://127.0.0.1:11434";
+  };
 }
