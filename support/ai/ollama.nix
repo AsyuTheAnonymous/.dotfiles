0f6222d48d99a67f156c06fc4e6ccc8e50370cf0
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-01-23 20:06:19 -0600

    Changed secret key to safe directory on vault

--- support/ai/ollama.nix
+++ support/ai/ollama.nix
@@ -16,9 +16,17 @@
     enable = true;
     port = 8080;
     stateDir = "/run/media/asyu/Vault/Web-ui";
-    environmentFile = "/home/asyu/.dotfiles/secret/openai.txt";
+    environmentFile = "/run/media/asyu/Vault/Web-ui/secret/openai.txt";
     environment = {
       WEBUI_AUTH = "False";
+      ENABLE_IMAGE_GENERATION = "True";
+      IMAGE_GENERATION_MODEL = "dall-e-3";
+      ENABLE_RAG_WEB_SEARCH = "True";
+      RAG_WEB_SEARCH_ENGINE = "duckduckgo";
+      AUDIO_STT_ENGINE = "openai";
+      AUDIO_STT_MODEL = "whisper-1";
+      AUDIO_TTS_ENGINE = "openai";
+      AUDIO_TTS_VOICE = "shimmer";
       OPENAI_API_KEY = "$(cat ~/.dotfiles/secret/openai.txt)";
 
     };
