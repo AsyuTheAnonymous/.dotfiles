1edb3cd39cbd9f13d125b3a95637389fe4528780
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-01-23 19:21:22 -0600

    Ollama configured!

--- support/ai/ollama.nix
+++ support/ai/ollama.nix
@@ -4,14 +4,30 @@
 {
   environment.systemPackages = with pkgs; [
     ollama
-
+    open-webui
   ];
+    # Ollama + Location
+  services.ollama = {
+    enable = true;
+    models = "/run/media/asyu/Vault/Models";
+  };
+  
+  services.open-webui ={
+    enable = true;
+    port = 8080;
+    stateDir = "/run/media/asyu/Vault/Web-ui";
+    environmentFile = "/home/asyu/.dotfiles/secret/openai.txt";
+    environment = {
+      WEBUI_AUTH = "False";
+      OPENAI_API_KEY = "$(cat ~/.dotfiles/secret/openai.txt)";
+
+    };
+  };
 
 
-
-
-
-
-
-
+  # Ollama Web Ui
+  # services.nextjs-ollama-llm-ui = {
+  #   enable = true;
+  #   ollamaUrl = "http://127.0.0.1:11434";
+  # };
 }
--- support/settings.nix
+++ support/settings.nix
@@ -7,11 +7,4 @@
     ./font.nix
     ./ai/ollama.nix
   ];
-  services.ollama = {
-    enable = true;
-    models = "/run/media/asyu/Vault/Models";
-  };
-
-
-
 }
