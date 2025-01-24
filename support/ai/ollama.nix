756c7f7acd524291ef18c806e270d410550cb86f
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-01-23 18:31:42 -0600

    Openweb-ui location on Vault disk for local ai storage

--- support/ai/ollama.nix
+++ support/ai/ollama.nix
@@ -4,15 +4,24 @@
 {
   environment.systemPackages = with pkgs; [
     ollama
+    open-webui
   ];
     # Ollama + Location
   services.ollama = {
     enable = true;
     models = "/run/media/asyu/Vault/Models";
   };
-  # Ollama Web Ui
-  services.nextjs-ollama-llm-ui = {
-    enable = true;
-    ollamaUrl = "http://127.0.0.1:11434";
+  
+  services.open-webui ={
+   enable = true;
+   port = 8080;
+   stateDir = "/run/media/asyu/Vault/Web-ui";
   };
+
+
+  # Ollama Web Ui
+  # services.nextjs-ollama-llm-ui = {
+  #   enable = true;
+  #   ollamaUrl = "http://127.0.0.1:11434";
+  # };
 }
