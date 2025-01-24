21e85bca234281cad4652660d625cae5167bda03
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-01-23 20:09:10 -0600

    Changing environment variables

--- support/ai/ollama.nix
+++ support/ai/ollama.nix
@@ -27,6 +27,7 @@
       AUDIO_STT_MODEL = "whisper-1";
       AUDIO_TTS_ENGINE = "openai";
       AUDIO_TTS_VOICE = "shimmer";
+      ENABLE_EVALUATION_ARENA_MODELS = "False";
       OPENAI_API_KEY = "$(cat ~/.dotfiles/secret/openai.txt)";
 
     };
