3953a18da4e998194f0105c7dc1c967db244cdcf
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-02-19 10:40:41 -0600

    Woah dude

--- system/network/nm.nix
+++ system/network/nm.nix
@@ -1,7 +1,7 @@
 { ... }:
 
 
-{etworkmanager12asklAL # Pick only one of the below networking options.
+{ # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
