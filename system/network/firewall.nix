89a299e07176f7a990a566c6c0d269e668827208
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-02-22 12:06:44 -0600

    firewall update

--- system/network/firewall.nix
+++ system/network/firewall.nix
@@ -3,7 +3,8 @@
 {
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 22 11434 3000 8080 80 ];
+    allowedTCPPorts = [ 22 11434 3000 8080 80 53317 ];
+    allowedUDPPorts = [ 53317 ];
     
   };
 
