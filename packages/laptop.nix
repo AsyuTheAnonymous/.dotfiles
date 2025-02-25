9296563f1621ed29dc29b32eea9ece162e74a147
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-02-25 14:45:19 -0600

    Nice uniform changes

---
+++ packages/laptop.nix
@@ -0,0 +1,31 @@
+{ pkgs, lib, ... }:
+
+
+{
+    # Sorrrrrrrrrrrrryyyyy
+  nixpkgs.config.allowUnfree = true;
+  
+  # Main packages for all my systems
+  environment.systemPackages = with pkgs; [
+    firefox
+    git
+    libreoffice
+    okular
+    neofetch
+    btop
+    pavucontrol
+    parsec-bin
+    protonmail-desktop
+    tree
+    localsend
+    vscode
+    nixd
+    nil
+    bottles
+    impression
+    gnome-boxes
+    unzip
+    unrar
+    zip
+  ];
+}
