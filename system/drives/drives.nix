3d56f697678eaa741aa1007bacdd5ee6b2a1a23b
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-03-02 14:09:55 -0600

    New drive fix?

--- system/drives/drives.nix
+++ system/drives/drives.nix
@@ -8,13 +8,15 @@
   fileSystems."/run/media/asyu/Games" = {
     device = "/dev/disk/by-uuid/4E385FD5385FBB23";
     fsType = "ntfs";
-    options = [ "auto" ];
+    options = [ "uid=1000" "gid=100" "fmask=0022" "dmask=0000" "windows_names" "big_writes" ];
+    # options = [ "auto" ];
   };
 
   fileSystems."/run/media/asyu/Vault" = {
     device = "/dev/disk/by-uuid/964C6CB04C6C8D35";
     fsType = "ntfs";
-    options = [ "auto" ];
+    options = [ "uid=1000" "gid=100" "fmask=0022" "dmask=0000" "windows_names" "big_writes" ];
+    # options = [ "auto" ];
   };
 }
 
