5c754be9b433931b4e2e280c8aa75346efa6d0a1
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-03-03 11:37:56 -0600

    OH MY GOD FINALLLLLLLLLY!

--- system/drives/drives.nix
+++ system/drives/drives.nix
@@ -8,7 +8,7 @@
   fileSystems."/run/media/asyu/Games" = {
     device = "/dev/disk/by-uuid/7c795867-f039-4de8-8279-30b2e7f4f826";
     fsType = "ext4";
-    options = [ "defaults" "noatime" "rw" "nofail" "users" ];
+    options = [ "defaults" "noatime" "rw" "nofail" "gid=1000" ];
   #  options = [ "defaults" "noatime" "rw" "uid=1000" "gid=100" ];
   };
 
