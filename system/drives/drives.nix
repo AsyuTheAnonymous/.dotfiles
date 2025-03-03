f4fb6282d82a9de99900577795e557e284c749e2
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-03-03 08:45:30 -0600

    drive changes

--- system/drives/drives.nix
+++ system/drives/drives.nix
@@ -3,19 +3,19 @@
 
 
 {
-#  boot.supportedFilesystems = [ "ntfs" ];
+ boot.supportedFilesystems = [ "ntfs" ];
 
-#  fileSystems."/mnt/Games" = {
- #   device = "/dev/disk/by-uuid/5cfc68e5-1858-4e9b-be84-bbfeb77767af";
-  #  fsType = "ext4";
-   # options = [ "defaults" "noatime" "rw" "uid=1000" "gid=1000" ];
- # };
+ fileSystems."/mnt/Games" = {
+   device = "/dev/disk/by-uuid/5cfc68e5-1858-4e9b-be84-bbfeb77767af";
+   fsType = "ext4";
+   options = [ "defaults" "noatime" "rw" "uid=1000" "gid=100" ];
+ };
 
-  #fileSystems."/mnt/Vault" = {
-   # device = "/dev/disk/by-uuid/964C6CB04C6C8D35";
-   # fsType = "ntfs";
-   # options = [ "uid=1000" "gid=1000" "fmask=0022" "dmask=0000" "windows_names" "big_writes" "rw" ];
- # };
+  fileSystems."/run/media/asyu/Vault" = {
+   device = "/dev/disk/by-uuid/964C6CB04C6C8D35";
+   fsType = "ntfs";
+   options = [ "uid=1000" "gid=100" "fmask=0022" "dmask=0000" "windows_names" "big_writes" "rw" ];
+ };
 }
 
 
