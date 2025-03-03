0e432c454f7de83d58442919679762c93066c559
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-03-03 12:09:25 -0600

    Dayuuuuuum

--- system/drives/drives.nix
+++ system/drives/drives.nix
@@ -19,23 +19,3 @@
   };
 }
 
-
-# {
-
-#   # Enable NTFS
-#   boot.supportedFilesystems = [ "ntfs" ];
-
-#   # More Drives
-#   fileSystems."/run/media/asyu/Games" = {
-#     device = "/dev/sdb2";
-#     fsType = "ntfs";
-#     options = [ "auto" ];
-#   };
-
-#   fileSystems."/run/media/asyu/Vault" = {
-#     device = "/dev/sda2";
-#     fsType = "ntfs";
-#     options = [ "auto" ];
-#   };
-
-# }
