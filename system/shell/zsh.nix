a1fec83cf162a12643a04bec20af42f4687b1384
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-03-14 00:28:50 -0500

    hm hard remove

--- system/shell/zsh.nix
+++ system/shell/zsh.nix
@@ -1,14 +1,9 @@
 { pkgs, ...}:
 
 {
-
-
-    # ZSH Default Shell
+  # ZSH Default Shell
   environment.shells = with pkgs; [ zsh ];
-  # May move this to a better location
   users.defaultUserShell = pkgs.zsh;
-
-  programs.starship.enable = true;
 
   programs.zsh = {
     enable = true;
@@ -22,22 +17,7 @@
       gc = "sudo nix-collect-garbage -d";
     };
     interactiveShellInit = ''
-      eval "$(${pkgs.starship}/bin/starship init zsh)"
       ${pkgs.neofetch}/bin/neofetch
     '';
   };
-  environment.etc = {
-    # "hypr/hyprland.conf".source = ../env/hypr/hyprland.conf;
-    # "hypr/hypridle.conf".source = ../env/hypr/hypridle.conf;
-    # "ghostty/config".source = ../env/hypr/configs/ghostty/config;
-    # "rofi/config.rasi".source = ../env/hypr/configs/rofi/config.rasi;
-    # "rofi/catppuccin-mocha.rasi".source = ../env/hypr/configs/rofi/catppuccin-mocha.rasi;
-    # "neofetch/config.conf".source = ../env/hypr/configs/neofetch/config.conf;
-    # "hypr/hyprpaper.conf".source = ../env/hypr/hyprpaper.conf;
-    "starship.toml".source = ../env/hypr/configs/starship/starship.toml;
-    # "waybar".source = ../env/hypr/configs/waybar;
-    # "vesktop/themes".source = ../../support/vesk-themes;
-  };
-    # export STARSHIP_CONFIG=${../../hosts/env/hypr/configs/starship/starship.toml}
-
 }
