ca97b040b72f7ec001baa74b0e4a2b9c3ddeb9cf
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-03-15 15:00:28 -0500

    my old aliases disappeared

--- system/shell/zsh.nix
+++ system/shell/zsh.nix
@@ -12,6 +12,8 @@
       unlock = "sudo chown asyu:users /home/asyu/.dotfiles/flake.lock";
       rebuild-desktop = "sudo nixos-rebuild switch --flake .#desktop";
       rebuild-laptop = "sudo nixos-rebuild switch --flake .#laptop";
+      hm-desktop = "home-manager switch --flake .#asyu@desktop";
+      hm-laptop = "home-manager switch --flake .#asyu@laptop";
       clean = "nix-env --delete-generations 5d";
       cleanhm = "home-manager expire-generations '-5 days'";
       gc = "sudo nix-collect-garbage -d";
