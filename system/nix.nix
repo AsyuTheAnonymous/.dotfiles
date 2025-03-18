ee95ea4f778b120cb7cf2793babe15c10721b142
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-03-18 18:57:37 -0500

    Unstable branch with zed on laptop

--- system/nix.nix
+++ system/nix.nix
@@ -3,7 +3,7 @@
 {
   nix = {
     settings = {
-      download-buffer-size = 10485760;
+      download-buffer-size = 16777216;
       http-connections = 100; # Increase concurrent downloads
       max-jobs = "auto"; # Set to number of CPU cores
       cores = 0; # Use all available cores
