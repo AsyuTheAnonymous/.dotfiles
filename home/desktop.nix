066f61026d0780f6f08c9ca87f170cdab26cc749
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-03-19 10:54:49 -0500

    moved things and deleted old comments

--- home/desktop.nix
+++ home/desktop.nix
@@ -45,31 +45,11 @@
   # Home Manager needs a bit of information about you and the paths it should manage.
   home.username = "asyu";
   home.homeDirectory = "/home/asyu";
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # Set-up Github repo
-  # programs.git= {
-  #   enable = true;
-  #   userName = "AsyuTheAnonymous";
-  #   userEmail = "asyutheanonymous@gmail.com";
-  #   extraConfig = {
-  #     init.defaultBranch = "main";
-  #     safe.directory = "/run/media/asyu/Vault";
-
-  #   };
-  # };
-
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
