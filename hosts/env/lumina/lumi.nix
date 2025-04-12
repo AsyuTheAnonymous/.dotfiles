f88b6f76a44f0f22ad125ffc90c848ca01bee91c
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-04-12 00:40:58 -0500

    should be good

--- hosts/env/lumina/lumi.nix
+++ hosts/env/lumina/lumi.nix
@@ -1,4 +1,6 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.xserver.enable = true;
   services.xserver.desktopManager.lumina.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 }
