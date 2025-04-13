dea4c750c0f0f2db8ce5b1eb6b2591c0e3d4febc
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-04-12 22:20:33 -0500

    phew

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -4,7 +4,7 @@
     ./support/gaming/gaming.nix
     ./support/flatpak/flatpak.nix
     ./ai/ollama.nix
-    ./support/tablet/tablet.nix
+    # ./support/tablet/tablet.nix
     ./support/virtualization/virtualization.nix
     ./support/git/git.nix
     ./emacs/emacs.nix
