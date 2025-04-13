6bb78a2ff36880ff91a0f6347bb839598f032391
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-04-12 22:37:41 -0500

    cleaning up. raspi will host open webui

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -3,7 +3,7 @@
     ./helpers/nh/nh.nix
     ./support/gaming/gaming.nix
     ./support/flatpak/flatpak.nix
-    ./ai/ollama.nix
+    # ./ai/ollama.nix
     # ./support/tablet/tablet.nix
     ./support/virtualization/virtualization.nix
     ./support/git/git.nix
