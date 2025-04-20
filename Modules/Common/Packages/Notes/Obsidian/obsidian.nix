53cb74836ee2158d3253d39c7e76cb46b48d26b9
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-04-20 18:01:20 -0500

    formatted.

--- Modules/Common/Packages/Notes/Obsidian/obsidian.nix
+++ Modules/Common/Packages/Notes/Obsidian/obsidian.nix
@@ -1,5 +1,10 @@
-{ config, lib, pkgs, ... }: { 
-    environment.systemPackages = with pkgs; [
-        obsidian
-    ];
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    obsidian
+  ];
 }
