19cb6de0d83404f310bbe7ff81335dbd255dffba
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-04-24 21:18:22 -0500

    yesm

--- System/Common/network/firewall.nix
+++ System/Common/network/firewall.nix
@@ -1,7 +1,7 @@
 {...}: {
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [22 11434 3000 8080 80 53317 9050];
+    allowedTCPPorts = [22 11434 3000 8080 80 53317 9050 443];
     allowedUDPPorts = [53317];
   };
 }
