236c088d6bf4ef6d2dbbc58c1d7f818a6a9b4553
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-05-02 14:26:09 -0500

    xfce module

---
+++ Hosts/Environments/XFCE/xfce.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, ... }:
+
+{
+  # Basic XFCE setup with disabled Xfwm4
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xfce = {
+        enable = true;
+        noDesktop = false;
+        enableXfwm = false;  # Disable XFCE's window manager
+      };
+    };
+    displayManager.lightdm.enable = true;
+    displayManager.defaultSession = "xfce";
+    
+    # NVIDIA configuration for RTX 3080
+    videoDrivers = [ "nvidia" ];
+  };
+  
+#   # NVIDIA specific settings
+#   nixpkgs.config.allowUnfree = true;
+#   hardware.nvidia = {
+#     modesetting.enable = true;
+#     powerManagement.enable = true;
+#     package = config.boot.kernelPackages.nvidiaPackages.stable;
+#     nvidiaSettings = true;
+#   };
+  
+#   # Hardware acceleration
+#   hardware.graphics.enable = true;
+#   hardware.opengl = {
+#     enable = true;
+#     driSupport = true;
+#     driSupport32Bit = true;
+#   };
+  
+  # Install Compiz
+  environment.systemPackages = with pkgs; [
+    compiz
+    compizConfig
+    
+    # Additional XFCE plugins
+    xfce.xfce4-whiskermenu-plugin
+    xfce.xfce4-pulseaudio-plugin
+    
+    # Themes
+    arc-theme
+    papirus-icon-theme
+  ];
+  
+  # Session startup commands to replace xfwm4 with compiz
+  services.xserver.desktopManager.xfce.extraSessionCommands = ''
+    # Kill xfwm4 if it's running and start compiz
+    pkill xfwm4 || true
+    compiz --replace ccp &
+  '';
+}
