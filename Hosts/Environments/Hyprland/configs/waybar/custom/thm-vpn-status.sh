d55528ffe719c091edda8dd0c3d3022a4217f749
AsyuTheAnonymous <asyutheanonymous@gmail.com>
2025-05-19 12:39:46 -0500

    Fixed the thm modules, THM is now integrated with waybar for seamless vpn connection

--- Hosts/Environments/Hyprland/configs/waybar/custom/thm-vpn-status.sh
+++ Hosts/Environments/Hyprland/configs/waybar/custom/thm-vpn-status.sh
@@ -2,7 +2,7 @@
 # File location: /home/asyu/.dotfiles/Hosts/Environments/Hyprland/configs/waybar/custom/thm-vpn-status.sh
 
 # VPN name in NetworkManager
-VPN_NAME="TryHackMe"
+VPN_NAME="thm"
 
 # Check if VPN is active
 if nmcli connection show --active | grep -q "$VPN_NAME"; then
